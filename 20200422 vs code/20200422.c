eb326915c585e3ef4821a65c5aa2d6707259cbdd
kaichieh0524 <ak850525@gmail.com>
2020-04-23 00:54:08 +0800

    20200422

---
+++ 20200422 vs code/20200422.c
@@ -0,0 +1,39 @@
+# include <stdio.h>
+
+int main(){
+    int integer1;
+    int integer2;
+    int sum;
+
+
+    printf("Please enter the first integer: ");
+    scanf("%d", &integer1);
+    printf("Please enter the second integer: ");
+    scanf("%d", &integer2);
+
+
+    sum = integer1 + integer2;
+    printf("Sum is %d.\n", sum);
+
+    /*  
+
+        use less memory
+
+        int integer, sum;
+
+        printf("Please enter the first integer: ");
+        scanf("%d", &integer);
+        sum = integer
+        printf("Please enter the second integer: ");
+        scanf("%d", &integer);
+        sum = sum + integer;
+        printf("Please enter the third integer: ");
+        scanf("%d", &integer);
+        sum = sum + integer;
+        printf("Sum is %d.\n", sum);
+        return 0;
+
+    */
+    return 0;
+
+}
