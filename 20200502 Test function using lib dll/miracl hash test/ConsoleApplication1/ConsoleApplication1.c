d3bebd2457bac1b7c85e1f9747c94c383914ef11
kaichieh0524 <ak850525@gmail.com>
2020-05-02 01:16:02 +0800

    test lib and dll

---
+++ 20200502 Test function using lib dll/miracl hash test/ConsoleApplication1/ConsoleApplication1.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include "miracl.h"
+
+char test[] = "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq";
+
+int main()
+{
+    char hash[20];
+    int i;
+    sha sh;
+    shs_init(&sh);
+    for (i = 0; test[i] != 0; i++) shs_process(&sh, test[i]);
+    shs_hash(&sh, hash);
+    for (i = 0; i < 20; i++) printf("%02x", (unsigned char)hash[i]);
+    printf("\n");
+    return 0;
+}
+
+
+
