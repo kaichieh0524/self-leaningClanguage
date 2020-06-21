4bd3fdddfec6f71a4975eb8116bb4cd20b578099
kaichieh0524 <ak850525@gmail.com>
2020-06-21 22:58:19 +0800

    intersection of two linked lists

--- Leetcode_challenge/20200621_6_Intersection of Two Linked Lists.c
+++ Leetcode_challenge/20200621_6_Intersection of Two Linked Lists.c
@@ -1,4 +1,6 @@
 //  Intersection of Two Linked Lists (easy)
+
+// Approach 1 (my first idea)
 // Runtime: 820 ms, faster than 6.29% of C online submissions for Intersection of Two Linked Lists.
 // Memory Usage: 13.5 MB, less than 72.32% of C online submissions for Intersection of Two Linked Lists.
 // It should be modified.
@@ -29,4 +31,22 @@ struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *he
         if (p2 == p1) {return p1;}
     }
     return NULL;
+}
+
+// Approach 2
+// Runtime: 32 ms, faster than 95.09% of C online submissions for Intersection of Two Linked Lists.
+// Memory Usage: 13.7 MB, less than 18.80% of C online submissions for Intersection of Two Linked Lists.
+
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    if (headA == NULL || headB == NULL) {return NULL;}
+   
+    struct ListNode* p1 = headA;
+    struct ListNode* p2 = headB;
+    
+    while (p1!=p2)
+    {
+        p1 = (p1 != NULL) ? p1 -> next : headB;
+        p2 = (p2 != NULL) ? p2 -> next : headA;
+    }
+    return p1;
 }
