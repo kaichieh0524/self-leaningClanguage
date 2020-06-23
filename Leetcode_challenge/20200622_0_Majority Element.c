11ba2f8f5986d92daf920b036ccdfb8c1202d57c
kaichieh0524 <ak850525@gmail.com>
2020-06-23 09:08:49 +0800

    majority element

--- Leetcode_challenge/20200622_0_Majority Element.c
+++ Leetcode_challenge/20200622_0_Majority Element.c
@@ -2,6 +2,8 @@
 
 // Runtime: 16 ms, faster than 94.74% of C online submissions for Majority Element.
 // Memory Usage: 7.5 MB, less than 59.21% of C online submissions for Majority Element.
+// key idea : if look the majority element and the other different element as a pair, then it will must be 
+// some majority element can not form a pair.
 
 int majorityElement(int* nums, int numsSize){   
     int i,IndexOfMajElem = 0, ctr = 1;
