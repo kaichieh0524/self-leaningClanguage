a8ae389dcef2dae02ab734a4e4b27113886ffb16
kaichieh0524 <ak850525@gmail.com>
2020-06-24 13:33:41 +0800

    contain duplicate

---
+++ Leetcode_challenge/20200624_0_Contains Duplicate.c
@@ -0,0 +1,92 @@
+// Contains Duplicate (easy)
+// Runtime: 40 ms, faster than 23.02% of C online submissions for Contains Duplicate.
+// Memory Usage: 8.1 MB, less than 78.05% of C online submissions for Contains Duplicate.
+// sort and find, the complexity is O(nlogn).
+// it needs to be modified
+
+void merge(int arr[], int l, int m, int r) 
+{ 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 =  r - m; 
+  
+    /* create temp arrays */
+    int L[n1], R[n2]; 
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1+ j]; 
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+  
+/* l is for left index and r is right index of the 
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r) 
+{ 
+    if (l < r) 
+    { 
+        // Same as (l+r)/2, but avoids overflow for 
+        // large l and h 
+        int m = l+(r-l)/2; 
+  
+        // Sort first and second halves 
+        mergeSort(arr, l, m); 
+        mergeSort(arr, m+1, r); 
+  
+        merge(arr, l, m, r); 
+    } 
+} 
+void printArray(int A[], int size) 
+{ 
+    int i; 
+    for (i=0; i < size; i++) 
+        printf("%d ", A[i]); 
+    printf("\n"); 
+}
+
+bool containsDuplicate(int* nums, int numsSize){
+    mergeSort(nums, 0, numsSize - 1);
+    printArray(nums, numsSize);
+    for (int i = 1 ; i < numsSize ; i++){
+        if (nums[i]==nums[i-1]){return 1;}
+    }
+    return 0;
+}
