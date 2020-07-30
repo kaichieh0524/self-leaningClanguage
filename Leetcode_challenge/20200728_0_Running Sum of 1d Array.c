9312f92d49ad222f2f12f7b61d773e653eaeff77
kaichieh0524 <ak850525@gmail.com>
2020-07-30 21:32:05 +0800

    sum of 1d array

---
+++ Leetcode_challenge/20200728_0_Running Sum of 1d Array.c
@@ -0,0 +1,16 @@
+// Running Sum of 1d Array (medium)
+// Runtime: 12 ms, faster than 26.89% of C online submissions for Running Sum of 1d Array.
+// Memory Usage: 6.3 MB, less than 100.00% of C online submissions for Running Sum of 1d Array.
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* runningSum(int* nums, int numsSize, int* returnSize){
+    int* ans = malloc(sizeof(int)*numsSize);
+    ans[0] = nums[0];
+    for (int i = 1; i < numsSize; i++){        
+        ans[i] = ans[i-1] + nums[i];
+    }
+    *returnSize = numsSize;
+    return ans;
+}
